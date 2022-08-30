e19f493b24a2d13aed183a4ff20cec991ac74d44
Logan Ricard <logvnjs@gmail.com>
2022-08-29 23:18:49 -0500

    added theme colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      SpaceMono: ['Space Mono', 'monospace'],
+    },
+    extend: {
+      colors: {
+        'git-blue': '#0079ff',
+        'light-grey': '#697c9a',
+        'light-navy': '#4b6a9b',
+        'dark-grey': '#2b3442',
+        'white-one': '#f6f8ff',
+        'white-two': '#fefefe',
+        'darker-navy': '#141d2f',
+        'dark-navy': '#1e2a47',
+      },
+    },
   },
   plugins: [],
 };
