6efcc280f3f9c7e04bf8272fd47a6c6d62217a58
Logan Ricard <logvnjs@gmail.com>
2022-09-12 12:46:13 -0500

    added No Results Photo

---
+++ components/NoResults/NoResults.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+
+const NoResults = () => {
+  return (
+    <>
+      <div className="relative w-[250px] h-[350px]">
+        <Image
+          src="/undraw_void_-3-ggu.svg"
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
+      <p className="font-semibold text-git-blue font-SpaceMono">
+        Sorry, User Not Found...
+      </p>
+    </>
+  );
+};
+
+export default NoResults;
